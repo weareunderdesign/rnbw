99ec6ae776352f5fd1d59442f99a0bf403abd7be
SoSo <sbsoso0411@gmail.com>
2022-11-17 16:17:34 +0200

    html node api declare

--- src/_node/html/apis.ts
+++ src/_node/html/apis.ts
@@ -1,5 +1,19 @@
 import { TTree } from '../';
 
+/**
+ * It will parse the content and build the tree data
+ * @param content 
+ * @returns 
+ */
 export const parseHtml = (content: string): TTree => {
   return {}
+}
+
+/**
+ * It will generate html string from the tree data
+ * @param tree 
+ * @returns 
+ */
+export const serializeHtml = (tree: TTree): string => {
+  return ''
 }
