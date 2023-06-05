0982cce573c1f9f71c614d73b92cd2c5cd1015a2
sbsoso0411 <sbsoso0411@gmail.com>
2023-06-05 02:41:41 +0200

    remove iframe opacity

--- src/components/main/stageView/iFrame/IFrame.tsx
+++ src/components/main/stageView/iFrame/IFrame.tsx
@@ -770,7 +770,7 @@ export const IFrame = (props: IFrameProps) => {
         <iframe
           ref={setContentRef}
           src={iframeSrc}
-          style={parseFileFlag ? { position: "absolute", width: "100%", height: "100vh" } : { position: "absolute", width: "100%", height: "100vh", overflow: 'hidden', opacity: 0.2 }}
+          style={parseFileFlag ? { position: "absolute", width: "100%", height: "100vh" } : { position: "absolute", width: "100%", height: "100vh", overflow: 'hidden'}}
         />}
     </>
   }, [iframeSrc, needToReloadIFrame, parseFileFlag, prevFileUid, setParseFile])
