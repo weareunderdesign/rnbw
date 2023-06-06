8bd9aa5b06408df427537daa7b409cd5394d7674
sbsoso0411 <sbsoso0411@gmail.com>
2023-06-07 01:52:15 +0200

    remove text selection in iframe

--- src/components/main/stageView/iFrame/styles.ts
+++ src/components/main/stageView/iFrame/styles.ts
@@ -24,4 +24,9 @@ body {
   min-width: 80px;
   min-height: 80px;
 }
+body > * {
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
 `
