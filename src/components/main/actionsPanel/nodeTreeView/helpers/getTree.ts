46b230d176a2692b5e6e323a07bc1e55060487a1
VictoriaShyika <victoriashyika@gmail.com>
2023-09-08 16:14:32 +0300

    added structuredClone

--- src/components/main/actionsPanel/nodeTreeView/helpers/getTree.ts
+++ src/components/main/actionsPanel/nodeTreeView/helpers/getTree.ts
@@ -1,6 +1,5 @@
-import _ from "lodash";
 import { TNodeTreeData } from "@_node/types";
 
 export const getTree = (nodeTree: TNodeTreeData): TNodeTreeData => {
-  return _.cloneDeep(nodeTree);
+  return structuredClone(nodeTree);
 };
