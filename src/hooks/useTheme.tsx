c10c30d86d231b88ca7565a76e78b24e3bdecd43
Atul Bhatt <38177419+atulbhatt-system32@users.noreply.github.com>
2023-09-08 13:24:06 +0530

    feat: add config for global custom hooks

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect, useContext } from "react";
+
+import { getSystemTheme } from "@_services/global";
+import { MainContext } from "@_redux/main";
+
+export const getCurrentTheme = (theme: string) => {
+  if (theme === "Dark") {
+    return "vs-dark";
+  } else if (theme === "Light") {
+    return "light";
+  } else {
+    return getSystemTheme();
+  }
+};
+
+export const useTheme = () => {
+  const { theme: _theme } = useContext(MainContext);
+  const [theme, setTheme] = useState<"vs-dark" | "light">();
+
+  useEffect(() => {
+    return () => {
+      setTheme(getCurrentTheme(_theme));
+    };
+  }, [_theme]);
+
+  return {
+    theme,
+  };
+};
