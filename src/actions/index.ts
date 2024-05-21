d4049bdbd2c151e657853de7647c74573ec57424
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-05-21 13:12:27 +0530

    feat: removed imports

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,4 @@
 import useTurnInto from "./useTurnInto.actions";
 import useFormatCode from "./useFormatCode.action";
-import useRemoveAllAttributes from "./useRemoveAllAttributes.action";
-import useRemoveEmptyFolders from "./useRemoveEmptyFolders.action";
 
-export {
-  useFormatCode,
-  useTurnInto,
-  useRemoveAllAttributes,
-  useRemoveEmptyFolders,
-};
+export { useFormatCode, useTurnInto };
