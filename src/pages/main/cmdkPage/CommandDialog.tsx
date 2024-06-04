f4b44ed233a44ce8fd44f11e66d58838b1c928cb
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-06-04 16:57:33 +0530

    feat: search file functionality in jumpstart

--- src/pages/main/cmdkPage/CommandDialog.tsx
+++ src/pages/main/cmdkPage/CommandDialog.tsx
@@ -24,6 +24,7 @@ import {
   PLACEHOLDERS,
 } from "./constants";
 import { SVGIcon, SVGIconIV } from "@_components/common";
+import { filterFiles } from "../helper";
 
 const iconMappping = {
   New: "/images/jumpstart/new.svg",
@@ -35,6 +36,7 @@ const iconMappping = {
   Autosave: "/images/jumpstart/autosave.svg",
   "Code Wrap": "/images/jumpstart/formatcode.svg",
   Recent: "/images/jumpstart/open.svg",
+  file: "/images/jumpstart/page.svg",
 };
 export const CommandDialog = ({ onClear, onJumpstart }: CommandDialogProps) => {
   const [currentFocusedMenuItem, setCurrentFocusedMenuItem] = useState({
@@ -180,6 +182,40 @@ export const CommandDialog = ({ onClear, onJumpstart }: CommandDialogProps) => {
     dispatch(setCmdkPages(["Actions"]));
     dispatch(setCmdkSearchContent(""));
   };
+  const getFilteredFiles = useCallback(() => {
+    const searchFilesArr = filterFiles(fileTree, cmdkSearchContent);
+    const filteredFiles = searchFilesArr.map((file) => {
+      const pathArrWithoutRootDir = file
+        .split("/")
+        .filter((item) => item !== "");
+
+      const pathWithoutRootDir = "/" + pathArrWithoutRootDir.slice(1).join("/");
+
+      return {
+        Featured: false,
+        Name: pathWithoutRootDir,
+        Path: file,
+        Icon: "", // Can add appropriate icon here
+        Description: "",
+        "Keyboard Shortcut": [
+          {
+            cmd: false,
+            shift: false,
+            alt: false,
+            key: "",
+            click: false,
+          },
+        ],
+        Group: "Files",
+        Context: "file",
+      };
+    });
+
+    return filteredFiles;
+  }, [fileTree, cmdkSearchContent]);
+
+  const filteredFiles = getFilteredFiles();
+
   return (
     <Command.Dialog
       open={cmdkOpen}
@@ -278,6 +314,35 @@ export const CommandDialog = ({ onClear, onJumpstart }: CommandDialogProps) => {
                   </Command.Group>
                 );
               })}
+              {currentCmdkPage === "Jumpstart" &&
+                filteredFiles?.map((file, index) => {
+                  return (
+                    <CommandItem
+                      key={`${file}-${index}`}
+                      command={{
+                        Featured: false,
+                        Name: file.Name,
+                        Path: file.Path,
+                        Icon: file.Icon, // Can add appropriate icon here
+                        Description: "",
+                        "Keyboard Shortcut": file["Keyboard Shortcut"],
+                        Group: file.Group,
+                        Context: file.Context,
+                      }}
+                      index={index}
+                      onSelect={async () => {
+                        navigate(file.Path);
+                        dispatch(setCmdkOpen(false));
+                      }}
+                      onMouseEnter={() => {
+                        setCurrentFocusedMenuItem({
+                          name: "file",
+                          description: file.Description || "",
+                        });
+                      }}
+                    />
+                  );
+                })}
             </Command.List>
           </div>
         </div>
