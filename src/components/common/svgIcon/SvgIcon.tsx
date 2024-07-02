162e0a0ef91a2f30fc1e6433b381dc3fe1dfeda4
Stanislava Shestakova <stanislavasalishcheva@yandex.ru>
2024-07-02 17:26:08 +0300

    Rollback

---
+++ src/components/common/svgIcon/SvgIcon.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo } from "react";
+
+import { SvgIconProps } from "./types";
+
+export const SVGIcon = (props: SvgIconProps) => {
+  const SVGIcon = useMemo<keyof JSX.IntrinsicElements>(() => {
+    return "svg-icon" as keyof JSX.IntrinsicElements;
+  }, []);
+  return <SVGIcon {...props}></SVGIcon>;
+};
+export const SVGIconI = (props: SvgIconProps) => {
+  const SVGIcon = useMemo<keyof JSX.IntrinsicElements>(() => {
+    return "svg-icon" as keyof JSX.IntrinsicElements;
+  }, []);
+  return <SVGIcon {...props}></SVGIcon>;
+};
+export const SVGIconII = (props: SvgIconProps) => {
+  const SVGIcon = useMemo<keyof JSX.IntrinsicElements>(() => {
+    return "svg-icon" as keyof JSX.IntrinsicElements;
+  }, []);
+  return <SVGIcon {...props}></SVGIcon>;
+};
+export const SVGIconIII = (props: SvgIconProps) => {
+  const SVGIcon = useMemo<keyof JSX.IntrinsicElements>(() => {
+    return "svg-icon" as keyof JSX.IntrinsicElements;
+  }, []);
+  return <SVGIcon {...props}></SVGIcon>;
+};
+
+export const SVGIconIV = ({ src, ...rest }: { src: string } & SvgIconProps) => {
+  const SVGIcon = useMemo<keyof JSX.IntrinsicElements>(() => {
+    return "svg-icon" as keyof JSX.IntrinsicElements;
+  }, []);
+
+  return <SVGIcon src={src} {...rest} />;
+};
