392c3de6a9445fac78557fae2cced6b8603798a3
Stanislava Shestakova <stanislavasalishcheva@yandex.ru>
2024-07-04 11:42:05 +0300

    Try to fix error

--- src/helper.ts
+++ src/helper.ts
@@ -25,7 +25,7 @@ import {
   TCmdkReference,
   TCmdkReferenceData,
   THtmlReferenceData,
-} from "@_types/main";
+} from "./types/main";
 import { AnyAction } from "@reduxjs/toolkit";
 import { THtmlNodeData } from "@_api/node";
 import { TFileNodeData, TFileNodeTreeData, createURLPath } from "@_api/index";
@@ -33,7 +33,7 @@ import {
   setActivePanel,
   setNavigatorDropdownType,
 } from "@_redux/main/processor";
-import { AnyFunction } from "./types";
+import { AnyFunction } from "./rnbw";
 import { toast } from "react-toastify";
 import { NavigateFunction } from "react-router-dom";
 import { TFilesReference } from "@rnbws/rfrncs.design";
