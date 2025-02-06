849e5ed8f29fe29f4931db44b19f0fe497b08895
atulbhatt-system32 <atulbhatt98@gmail.com>
2025-02-06 14:09:03 +0530

    parsing error status bar added

--- src/codeView/CodeView.tsx
+++ src/codeView/CodeView.tsx
@@ -4,6 +4,7 @@ import React, {
   useEffect,
   useMemo,
   useRef,
+  useState,
 } from "react";
 
 import * as monaco from "monaco-editor";
@@ -27,6 +28,8 @@ import { useEditor } from "./hooks";
 import { getNodeUidByCodeSelection } from "./helpers";
 import { setEditingNodeUidInCodeView } from "@_redux/main/codeView";
 import { getFileExtension } from "../sidebarView/navigatorPanel/helpers";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { ParsingErrorsPanel } from "@src/features/codeView/ParsingErrorsPanel";
 
 loader.config({ monaco });
 
@@ -61,10 +64,11 @@ export default function CodeView() {
     updateLanguage,
 
     editorConfigs,
-
+    parsingErrors,
     codeSelection,
   } = useEditor();
 
+  const [showParsingErrors, setShowParsingErrors] = useState(false);
   monacoEditorRef.current?.onKeyDown(handleKeyDown);
 
   const onPanelClick = useCallback(() => {
@@ -295,46 +299,93 @@ export default function CodeView() {
     editorModel.deltaDecorations(oldDecorations, newDecorations);
   }, [nodeUidPositions]);
 
+  const handleErrorClick = useCallback(
+    (line: number, column: number) => {
+      if (monacoEditorRef.current) {
+        monacoEditorRef.current.revealPositionInCenter({
+          lineNumber: line,
+          column: column,
+        });
+        monacoEditorRef.current.setPosition({
+          lineNumber: line,
+          column: column,
+        });
+        monacoEditorRef.current.focus();
+      }
+    },
+    [monacoEditorRef],
+  );
+
+  const toggleParsingErrors = useCallback(() => {
+    setShowParsingErrors(!showParsingErrors);
+  }, [showParsingErrors]);
   return useMemo(() => {
     return (
-      <>
-        <div
-          id="CodeView"
-          onDragCapture={(e) => {
-            e.preventDefault();
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
-          }}
-          style={{
-            width: "100%",
-            height: "100%",
-            zIndex: 999,
-            overflow: "hidden",
-            ...(codeErrors
-              ? {
-                  outlineWidth: "1px",
-                  outlineStyle: "solid",
-                  outlineOffset: "-1px",
-                  outlineColor: "var(--color-negative)",
-                }
-              : {}),
-            transition: "0.3s all",
-            borderLeft: 0,
-          }}
-          className={`border-left background-primary ${codeErrors && "border"}`}
-          onClick={onPanelClick}
-        >
-          <Editor
-            onMount={handleEditorDidMount}
-            theme={theme}
-            language={language}
-            value={currentFileContent}
-            onChange={(value) => handleOnChange(value, currentFileUid)}
-            options={editorConfigs}
-          />
-        </div>
-      </>
+      <div style={{ position: "relative", height: "100%" }}>
+        <PanelGroup direction="vertical" autoSaveId="CodeView">
+          <Panel defaultSize={90} minSize={20} order={1}>
+            <div
+              id="CodeView"
+              onDragCapture={(e) => {
+                e.preventDefault();
+              }}
+              onDragOver={(e) => {
+                e.preventDefault();
+              }}
+              style={{
+                width: "100%",
+                height: "100%",
+                zIndex: 999,
+                overflow: "hidden",
+                ...(codeErrors
+                  ? {
+                      outlineWidth: "1px",
+                      outlineStyle: "solid",
+                      outlineOffset: "-1px",
+                      outlineColor: "var(--color-negative)",
+                    }
+                  : {}),
+                transition: "0.3s all",
+                borderLeft: 0,
+              }}
+              className={`border-left background-primary ${codeErrors && "border"}`}
+              onClick={onPanelClick}
+            >
+              <Editor
+                onMount={handleEditorDidMount}
+                theme={theme}
+                language={language}
+                value={currentFileContent}
+                onChange={(value) => handleOnChange(value, currentFileUid)}
+                options={editorConfigs}
+              />
+            </div>
+          </Panel>
+
+          {showParsingErrors && (
+            <>
+              <PanelResizeHandle />
+
+              <Panel defaultSize={10} order={2}>
+                <ParsingErrorsPanel
+                  errors={parsingErrors}
+                  onErrorClick={handleErrorClick}
+                />
+              </Panel>
+            </>
+          )}
+
+          <div className="row padding-xs background-secondary">
+            {parsingErrors.length > 0 ? (
+              <span onClick={toggleParsingErrors} className="padding-xs">
+                ⚠️
+              </span>
+            ) : (
+              <span className="padding-xs">✅</span>
+            )}
+          </div>
+        </PanelGroup>
+      </div>
     );
   }, [
     onPanelClick,
